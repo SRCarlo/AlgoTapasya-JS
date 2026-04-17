88c6bf0e7858cb2caf4fb7ca8bde255a1baac9f0
SRCarlo <asphaltshubhuu@gmail.com>
2026-04-17 17:46:35 +0530

    Day 4: Subarray Sum Equals K
    - Applied prefix sum with hashmap
    - Counted subarrays efficiently
    - Avoided nested loops
    - Practiced cumulative sum logic

---
+++ Week-01-Arrays-Basics/Day-04/03-subarray-sum-k.js
@@ -0,0 +1,45 @@
+/*
+ * Name: Subarray Sum Equals K
+
+ * Approach: Prefix Sum + Hash Map
+
+ * Input: nums = [1,1,1], k = 2
+
+ * Output: 2
+
+ * Explanation:
+ * Count subarrays where sum = k
+ * 
+ * Algorithm Used: Prefix Sum + Hashing
+ * 
+ * Key Learning: Store cumulative sums
+ * 
+ * Time Complexity: O(n)
+ * 
+ * Space Complexity: O(n)
+ * 
+ */
+
+function subarraySum(nums, k) {
+  let map = new Map();
+  map.set(0, 1);
+
+  let sum = 0;
+  let count = 0;
+
+  for (let num of nums) {
+    sum += num;
+
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
+    }
+
+    map.set(sum, (map.get(sum) || 0) + 1);
+  }
+
+  return count;
+}
+
+// Test Cases
+console.log(subarraySum([1, 1, 1], 2)); // 2
+console.log(subarraySum([1, 2, 3], 3)); // 2
