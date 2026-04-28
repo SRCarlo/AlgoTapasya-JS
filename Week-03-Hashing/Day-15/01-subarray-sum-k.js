05e5cb52013e26cce07cbcd4087c1c6ab078ef89
SRCarlo <asphaltshubhuu@gmail.com>
2026-04-28 15:21:55 +0530

    Day 15: Subarray Sum Equals K
    - Used prefix sum with hashmap
    - Applied sum-k pattern
    - Counted valid subarrays
    - Strengthened prefix logic

---
+++ Week-03-Hashing/Day-15/01-subarray-sum-k.js
@@ -0,0 +1,46 @@
+/*
+ *
+ * Name: Subarray Sum Equals K
+ *
+ * Approach: Prefix Sum + HashMap
+ *
+ * Input: nums = [1,1,1], k = 2
+ *
+ * Output: 2
+ *
+ * Explanation:
+ * Store prefixSum frequency
+ *
+ * Algorithm Used: Prefix Sum
+ *
+ * Key Learning: prefixSum - k pattern
+ *
+ * Time Complexity: O(n)
+ *
+ * Space Complexity: O(n)
+ *
+ */
+
+function subarraySum(nums, k) {
+  let map = new Map();
+  map.set(0, 1);
+
+  let sum = 0;
+  let count = 0;
+
+  for (let num of nums) {
+    sum += num;
+
+    if (map.has(sum - k)) {
+      count += map.get(sum - k);
+    }
+
+    map.set(sum, (map.get(sum) || 0) + 1);
+  }
+
+  return count;
+}
+
+// Test Cases
+console.log(subarraySum([1, 1, 1], 2)); // 2
+console.log(subarraySum([1, 2, 3], 3)); // 2
